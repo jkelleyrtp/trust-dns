40c1119bb13c4942f37b34057c59ecd5e0cf2185
Noah Kennedy <nkennedy@cloudflare.com>
2022-04-27 15:53:18 -0500

    maybe

--- crates/server/src/server/server_future.rs
+++ crates/server/src/server/server_future.rs
@@ -691,8 +691,12 @@ impl<T: RequestHandler> ServerFuture<T> {
 
         let mut join_set = self.join_set;
 
-        if let Some(_permit) = maybe_permit {
-            join_set.shutdown().await;
+        if let Some(permit) = maybe_permit {
+            tokio::spawn(async move {
+                let guard = permit;
+                join_set.shutdown().await;
+                drop(guard);
+            });
         }
 
         match result {
